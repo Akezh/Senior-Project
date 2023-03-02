7f91f674925186e1e5357cac91c43de031e16996
Temirzhan Yussupov <timka2609@gmail.com>
2023-03-02 14:27:53 +0600

    feat: add create problem button

--- src/components/organisms/Header/component.tsx
+++ src/components/organisms/Header/component.tsx
@@ -106,7 +106,7 @@ export const Header: FC = () => {
                   href="#"
                   className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Contact
+                  <Link href="/problem/create">Create problem</Link>
                 </a>
               </li>
             </ul>
